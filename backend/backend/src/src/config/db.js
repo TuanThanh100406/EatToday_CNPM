b5dad021de771155201dd1d219b45741507aab16
TuanThanh100406 <thanhlehok100406@gmail.com>
2025-08-21 13:54:51 +0700

    Update db.js

--- backend/backend/src/src/config/db.js
+++ backend/backend/src/src/config/db.js
@@ -1,1 +1,26 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected");
+  } catch (err) {
+    console.error("❌ DB connection error:", err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = { sequelize, connectDB };
 
